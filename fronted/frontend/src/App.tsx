61b839e364fbd1c1bbb62ea91aac77a1f8a7ee25
Zulozo <iuri.avengers@gmail.com>
2022-07-17 17:03:45 -0300

    first component

--- fronted/frontend/src/App.tsx
+++ fronted/frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components/NotificationButon"
 
 
 function App() {
-  return(
-    <h1>Ola mundo!</h1>
+  return (
+    <>
+      <h1>Ola mundo!</h1>
+      <NotificationButton />
+    </>
   )
 }
 
